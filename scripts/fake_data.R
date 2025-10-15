b2c56611b747ed19089a97d87631ecf0c8db6a13
January Weiner <january.weiner@gmail.com>
2025-10-15 09:45:09 +0200

    added data

--- scripts/fake_data.R
+++ scripts/fake_data.R
@@ -2,9 +2,8 @@
 # t.test - I will use the iris data set, actually.
 
 library(tidyverse)
-library(writexl)
 
 iris |>
   select("Sepal_Length" = Sepal.Length, Species) |>
   filter(Species %in% c("setosa", "versicolor")) |>
-  write_xlsx("iris.xlsx")
+  write.csv("iris.csv")
